e2cef274a27347d17b2cf21438fb234a19dbc03d
RunDTM <68605849+RunDTM@users.noreply.github.com>
2021-12-22 22:15:27 +0500

    Add files via upload

---
+++ Discord AutoJoiner/MainForm.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Discord_AutoJoiner
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.joinButton = new System.Windows.Forms.Button();
+            this.inviteBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // joinButton
+            // 
+            this.joinButton.Location = new System.Drawing.Point(12, 41);
+            this.joinButton.Name = "joinButton";
+            this.joinButton.Size = new System.Drawing.Size(608, 23);
+            this.joinButton.TabIndex = 0;
+            this.joinButton.Text = "Join";
+            this.joinButton.UseVisualStyleBackColor = true;
+            this.joinButton.Click += new System.EventHandler(this.joinButton_Click);
+            // 
+            // inviteBox
+            // 
+            this.inviteBox.Location = new System.Drawing.Point(13, 13);
+            this.inviteBox.Name = "inviteBox";
+            this.inviteBox.Size = new System.Drawing.Size(607, 20);
+            this.inviteBox.TabIndex = 1;
+            this.inviteBox.Text = "Invite";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(632, 75);
+            this.Controls.Add(this.inviteBox);
+            this.Controls.Add(this.joinButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Discord AutoJoiner";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button joinButton;
+        private System.Windows.Forms.TextBox inviteBox;
+    }
+}
+
---
+++ Discord AutoJoiner/MainForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.Net;
+using System.Text;
+
+namespace Discord_AutoJoiner
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void joinButton_Click(object sender, EventArgs e)
+        {
+            var values = new Dictionary<string, dynamic> { { "cmd", "INVITE_BROWSER" }, { "args", new Dictionary<string, string> { {"code", "WbYybJZGTz"} }  }, { "nonce", Guid.NewGuid().ToString() } };
+            var content = JsonConvert.SerializeObject(values);
+            var request = WebRequest.Create("http://127.0.0.1:6463/rpc?v=1");
+            request.Method = "POST";
+            byte[] byteArray = Encoding.UTF8.GetBytes(content);
+            request.ContentType = "application/json";
+            request.ContentLength = byteArray.Length;
+            request.Headers.Add("Origin", "https://discord.com");
+            var reqStream = request.GetRequestStream();
+            reqStream.Write(byteArray, 0, byteArray.Length);
+            request.GetResponse();
+        }
+    }
+}
